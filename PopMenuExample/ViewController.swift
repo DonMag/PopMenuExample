29629c88250067d0d31688e943631a6c4f8ab486
DonMag <ACP@ACPs-iMac.local>
2016-11-04 10:44:42 -0400

    anchor popup to 'normal' UIButton

--- PopMenuExample/ViewController.swift
+++ PopMenuExample/ViewController.swift
@@ -48,6 +48,37 @@ class ViewController: UIViewController, UIPopoverPresentationControllerDelegate,
 		
 	}
 	
+	@IBAction func regularMenuButtonTapped(sender: AnyObject) {
+		
+		guard let btn = sender as? UIButton else { return }
+
+		// instantiate the popover menu view controller
+		let vc = storyboard?.instantiateViewControllerWithIdentifier("PopMenuViewController") as! PopMenuViewController
+		
+		// set the desired size and background color
+		vc.preferredContentSize = CGSize(width: 100, height: 170)
+		vc.view.backgroundColor = UIColor.lightGrayColor()
+		
+		// set the custom CalendarTypePopoverDelegate to self
+		vc.menuDelegate = self
+		
+		// set the popover "styling"
+		vc.modalPresentationStyle = .Popover
+		
+		vc.popoverPresentationController?.backgroundColor = UIColor.lightGrayColor()
+		
+		// tell it what object to use as the popover "pointy thingy" reference
+		vc.popoverPresentationController?.sourceView = self.view
+		vc.popoverPresentationController?.sourceRect = btn.frame
+		
+		// set the UIPopoverPresentationControllerDelegate to self
+		vc.popoverPresentationController?.delegate = self
+		
+		// show the popover menu
+		presentViewController(vc, animated: true, completion: nil)
+		
+	}
+	
     @IBAction func menuButtonTapped(sender: AnyObject) {
 		
 		// instantiate the popover menu view controller
